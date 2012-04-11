9554129df798be23c1d755fd4ee8ddb084e266f2
Ken Cochrane <kencochrane@gmail.com>
2012-04-11 09:44:40 -0400

    fixed STATICFILES_DIRS path issue

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -72,7 +72,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'hellodjango/static/'),
+    os.path.join(PROJECT_ROOT, 'static/'),
 )
 
 # List of finder classes that know how to find static files in
