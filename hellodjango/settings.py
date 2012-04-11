427301cea8a11edb1fa12fa4bd21904030986c63
Ken Cochrane <kencochrane@gmail.com>
2012-04-11 09:35:44 -0400

    Added the static directory, error pages, robots.txt, and favicon.ico

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -72,9 +72,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'hellodjango/static/'),
 )
 
 # List of finder classes that know how to find static files in
