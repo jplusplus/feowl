8514df6df400887781bf059bf29b70e4bba9f555
Alexander Slabiak <alex@tesobe.com>
2012-05-23 18:00:52 +0200

    Add some comments

--- django/feowl/tests.py
+++ django/feowl/tests.py
@@ -8,6 +8,9 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
+# Need for later:
+#  = HelloWorld(text_1 = 'Hello', text_2 = 'World', int_1 = 1, geographic_point_1 = Point(151.207555, -33.88576), date_1 = timezone.now())
+
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
