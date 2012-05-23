694593478335a74763d8712012dfb148ca02c8da
Alexander Slabiak <alex@tesobe.com>
2012-05-23 13:12:04 +0200

    Fix path

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'hellodjango')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'hellodjango.settings'
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'django')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
