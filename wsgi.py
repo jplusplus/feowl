905b7afb5bb1b8ed1f15a930c59d50929325b257
Alexander Slabiak <alex@tesobe.com>
2012-05-23 13:16:37 +0200

    Change wsgi to use django 1.4

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'django')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'django.settings'
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
