67db82e2808396962db3625178016c67d01a2eed
Alexander Slabiak <alex@tesobe.com>
2012-05-23 14:12:36 +0200

    add django folder to file namespace

--- createdb.py
+++ createdb.py
@@ -1,7 +1,10 @@
 import MySQLdb
 import psycopg2
 import os
-#from wsgi import *
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'django')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django.settings'
 
 
 def create_dbs():
