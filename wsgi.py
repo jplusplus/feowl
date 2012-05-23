b0122dd663b11df38a2e0eddc79c6f57c92f0482
Alexander Slabiak <alex@tesobe.com>
2012-05-23 13:36:28 +0200

    Change import

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,5 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'django')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'django.settings'
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import *
 application = get_wsgi_application()
