edd2d663e4f04d388f5fd8d71c3ffa39cc1d5b7d
pirhoo <pirhoo@naboo.(none)>
2012-06-05 12:39:43 +0200

    Added Smarty engine module line

--- UI/application/bootstrap.php
+++ UI/application/bootstrap.php
@@ -98,15 +98,16 @@ Kohana::$config->attach(new Config_File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
-	 'auth'       => MODPATH.'auth',       // Basic authentication
 	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
 	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-	 'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
-	 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
-	 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
-	));
+	'auth'       => MODPATH.'auth',       // Basic authentication
+	'database'   => MODPATH.'database',   // Database access
+	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+	'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+	'smarty' 	 => MODPATH.'smarty'	  // Smarty Template Engine
+));
 
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
