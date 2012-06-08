e45593daf883cfc69e77bbe775ac1c4a5cf75599
Stefan Bethge <stefan@tesobe.com>
2012-06-08 18:31:50 +0200

    allow filtering,
    urls for new resources,
    set allowed methods for different resources,
    fix decimal field default values,
    make test.html work again (with related resource!)

--- API/feowl-api/feowl/models.py
+++ API/feowl-api/feowl/models.py
@@ -8,7 +8,7 @@ class UserProfile(models.Model):
     """Models for the User"""
 
     user = models.OneToOneField(User)
-    credibility = models.DecimalField(max_digits=3, decimal_places=2, default=1.00)
+    credibility = models.DecimalField(max_digits=3, decimal_places=2, default='1.00')
     language = models.CharField(max_length=5, default="EN")
 
 
@@ -22,14 +22,14 @@ class Device(models.Model):
 
 class Area(models.Model):
     """Models for the Area"""
-
+    
     objects = models.GeoManager()
 
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     pop_per_sq_km = models.DecimalField(max_digits=8, decimal_places=2)
-    geometry = models.GeometryField()
+    geometry = models.GeometryField(srid=4326, geography=False)
 
     def kml(self):
         return self.geometry.kml
@@ -50,10 +50,10 @@ class Report(models.Model):
     #stay with geometries since they support more postgis functions
     #see: http://postgis.refractions.net/documentation/manual-1.5/ch04.html#PostGIS_GeographyVSGeometry
     location = models.PointField(srid=4326, geography=False, blank=True, null=True, help_text="String in form of POINT(lon, lat)")
-    quality = models.DecimalField(max_digits=3, decimal_places=2, default=1.00)
+    quality = models.DecimalField(max_digits=3, decimal_places=2, default='1.00')
     deleted = models.BooleanField(default=False)
     flagged = models.BooleanField(default=False)
     #data = JSONField(blank=True, help_text="additional fields corresponding to the type in JSON format")
-    area = models.ForeignKey(Area)
+    area = models.ForeignKey(Area, blank=False, null=False)
     user = models.ForeignKey(User, blank=True, null=True)
     device = models.ForeignKey(Device, blank=True, null=True)
