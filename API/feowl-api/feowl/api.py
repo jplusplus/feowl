dbfed624029df95f73b0c25135517bf8436f7cc5
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2012-06-15 17:25:42 +0200

    Updated the Api with the new authenticaitoin and add a extra resource with toOnefield to access the profile

--- API/feowl-api/feowl/api.py
+++ API/feowl-api/feowl/api.py
@@ -3,7 +3,8 @@
 from tastypie import fields
 from tastypie.resources import Resource, ModelResource, ALL
 from tastypie.authorization import DjangoAuthorization
-from tastypie.authentication import ApiKeyAuthentication
+# from tastypie.authentication import ApiKeyAuthentication
+from tastypie_auth import ConfigurableApiKeyAuthentication
 # from django.contrib.gis.geos import Point
 from models import PowerReport, Device, UserProfile, Area
 
@@ -11,6 +12,9 @@ from django.conf.urls.defaults import url
 from django.contrib.auth.models import User
 from django.db import models
 from tastypie.models import create_api_key
+
+from django.db import IntegrityError
+from tastypie.exceptions import BadRequest
 
 models.signals.post_save.connect(create_api_key, sender=User)
 
@@ -20,7 +24,7 @@ class DeviceResource(ModelResource):
         queryset = Device.objects.all()
         resource_name = 'devices'
 
-        authentication = ApiKeyAuthentication()
+        authentication = ConfigurableApiKeyAuthentication(username_param='user_name')
         authorization = DjangoAuthorization()
 
         fields = ['category', 'phone_number']
@@ -29,22 +33,55 @@ class DeviceResource(ModelResource):
         detail_allowed_methods = ['get']
 
 
-class UserResource(ModelResource):
+class UserProfileResource(ModelResource):
     class Meta:
         queryset = UserProfile.objects.all()
-        resource_name = 'users'
+        resource_name = 'userprofiles'
+        include_resource_uri = False
+        include_absolute_url = False
 
-        authentication = ApiKeyAuthentication()
+        authentication = ConfigurableApiKeyAuthentication(username_param='user_name')
         authorization = DjangoAuthorization()
 
-        fields = ['username', 'credibility', 'language']
+        fields = ['credibility', 'language']
 
         list_allowed_methods = ['get', 'post']
         detail_allowed_methods = ['get', 'put']
 
         filtering = {
-            'username': ALL
+            "credibility": ALL,
+            "language": ALL,
         }
+
+
+class UserResource(ModelResource):
+    profile = fields.ToOneField(UserProfileResource, 'get_profile', full=True)
+
+    class Meta:
+        resource_name = 'users'
+        queryset = User.objects.all()
+
+        authentication = ConfigurableApiKeyAuthentication(username_param='user_name')
+        authorization = DjangoAuthorization()
+
+        fields = ['email', 'password', 'username']
+
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get', 'put']
+
+        filtering = {
+            "username": ALL,
+            "email": ALL,
+        }
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        try:
+            bundle = super(UserResource, self).obj_create(bundle, request, **kwargs)
+            bundle.obj.set_password(bundle.data.get('password'))
+            bundle.obj.save()
+        except IntegrityError:
+            raise BadRequest('That username already exists')
+        return bundle
 
 
 class AreaResource(ModelResource):
@@ -71,7 +108,7 @@ class PowerReportResource(ModelResource):
 
         #danger: this exposes all permissions for anybody, switch to use api key auth soon
         #see: http://www.infoq.com/news/2010/01/rest-api-authentication-schemes
-        authentication = ApiKeyAuthentication()
+        authentication = ConfigurableApiKeyAuthentication(username_param='user_name')
         authorization = DjangoAuthorization()
 
         #whitelist of fields to be public
