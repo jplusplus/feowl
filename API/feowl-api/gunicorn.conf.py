715428b050743eaf7e68a701024f0c6f70a8baf6
Alexander Slabiak <alex@tesobe.com>
2012-06-20 13:00:44 +0200

    Some extra love for the gunicorn.conf

--- API/feowl-api/gunicorn.conf.py
+++ API/feowl-api/gunicorn.conf.py
@@ -2,16 +2,17 @@ from re import search
 from os import getcwd
 
 # Check for dev in path, dev will bind the port to 8001, live to 8000
-if search('dev', getcwd()).group() == 'dev':
+seaching_path = 'dev'
+if search(seaching_path, getcwd()).group() == seaching_path:
     port = '8001'
+    logfile = '/var/log/gunicorn/gunicorn-%s.log' % seaching_path
+    proc_name = 'gunicorn-%s' % seaching_path
 else:
     port = '8000'
+    logfile = '/var/log/gunicorn/gunicorn.log'
 
 bind = '127.0.0.1:' + port
 backlog = 2048
-logfile = '/var/log/gunicorn/gunicorn.log'
 loglevel = 'info'
 
 daemon = True
-
-proc_name = 'gunicorn'
