a07492122e8d940e1234b58145624f927e4a1064
Tim Kleinschmidt <timk@tesobe.com>
2012-06-25 12:54:16 +0200

    Added Userprofile as a Inline to Useradmin interface

--- API/feowl-api/feowl/admin.py
+++ API/feowl-api/feowl/admin.py
@@ -6,16 +6,20 @@ from tastypie.models import ApiAccess, ApiKey
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
+
+class UserProfileInline(admin.TabularInline):
+    model = UserProfile
+
+
+class UserModelAdmin(UserAdmin):
+    inlines = UserAdmin.inlines + [UserProfileInline, ApiKeyInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserModelAdmin)
+
 admin.site.register(PowerReport, admin.OSMGeoAdmin)
 admin.site.register(Area, admin.OSMGeoAdmin)
 admin.site.register(Device)
-admin.site.register(UserProfile)
 
 admin.site.register(ApiKey)
 admin.site.register(ApiAccess)
-
-class UserModelAdmin(UserAdmin):
-    inlines = UserAdmin.inlines + [ApiKeyInline]
-
-admin.site.unregister(User)
-admin.site.register(User,UserModelAdmin)
