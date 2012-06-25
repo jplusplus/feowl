644d10427474f15c2eac204a2402db5f0a07dcf4
Tim Kleinschmidt <timk@tesobe.com>
2012-06-25 18:53:16 +0200

    Added device resourcce test to the test.py

--- API/feowl-api/feowl/tests.py
+++ API/feowl-api/feowl/tests.py
@@ -1,6 +1,6 @@
 from django.utils import unittest
 from django.test.client import Client
-from models import PowerReport, Area, UserProfile
+from models import PowerReport, Area, UserProfile, Device
 from django.contrib.auth.models import User
 from tastypie_test import ResourceTestCase
 from django.db import models
@@ -329,3 +329,116 @@ class UserResourceTest(ResourceTestCase):
     def test_delete_detail(self):
         """Delete a single user is not allowed from the API with authenticated"""
         self.assertHttpMethodNotAllowed(self.c.delete(self.detail_url, self.get_credentials()))
+
+
+class DeviceResourceTest(ResourceTestCase):
+
+    def setUp(self):
+        super(DeviceResourceTest, self).setUp()
+
+        # Create a user.
+        self.username = u'john'
+        self.password = u'doe'
+        self.email = u'john@example.com'
+        self.user = User.objects.create_user(self.username, self.email, self.password)
+        self.api_key = self.user.api_key.key
+        self.c = Client()
+        self.post_data = {
+            'category': 'SubDevice',
+            'phone_number': '12381294323',
+        }
+        self.put_data = {
+            'phone_number': "4267486238"
+        }
+
+        Device(phone_number="01234567890", category="MainDevice", user=self.user).save()
+        # Fetch the ``Entry`` object we'll use in testing.
+        # Note that we aren't using PKs because they can change depending
+        # on what other tests are running.
+        self.device_1 = Device.objects.get(pk=1)
+
+        # We also build a detail URI, since we will be using it all over.
+        # DRY, baby. DRY.
+        self.list_url = '/api/v1/devices/'
+        self.detail_url = '{0}{1}/'.format(self.list_url, self.device_1.pk)
+
+    def get_credentials(self):
+        return {"user_name": self.username, "api_key": self.api_key}
+
+    def test_get_list_unauthorzied(self):
+        """Get devices from the API without authenticated"""
+        self.assertHttpUnauthorized(self.c.get(self.list_url))
+
+    def test_get_list_json(self):
+        """Get devices from the API with authenticated. With checks if all keys are available"""
+        resp = self.c.get(self.list_url, self.get_credentials())
+        self.assertValidJSONResponse(resp)
+
+        # Scope out the data for correctness.
+        self.assertEqual(len(self.deserialize(resp)['objects']), 1)
+        # Here, we're checking an entire structure for the expected data.
+        self.assertEqual(self.deserialize(resp)['objects'][0], {
+            "category": "MainDevice",
+            "phone_number": "01234567890",
+            "resource_uri": self.detail_url
+        })
+
+    def test_get_detail_unauthenticated(self):
+        """Try to Get a single device from the API without authenticated"""
+        self.assertHttpUnauthorized(self.c.get(self.detail_url))
+
+    def test_get_detail_json(self):
+        """Get a single device from the API with authenticated. With checks if all keys are available"""
+        resp = self.c.get(self.detail_url, self.get_credentials())
+        self.assertValidJSONResponse(resp)
+
+        # We use ``assertKeys`` here to just verify the keys, not all the data.
+        self.assertKeys(self.deserialize(resp), ['category', 'phone_number', 'resource_uri'])
+        self.assertEqual(self.deserialize(resp)['category'], "MainDevice")
+
+    def test_post_list_unauthenticated(self):
+        """Try to Post a single device to the API without authenticated"""
+        self.assertHttpUnauthorized(self.c.post(self.list_url, data=self.post_data))
+
+    def test_post_list_without_permissions(self):
+        """Try to Post a single device to the API with authenticated and without permission"""
+        self.assertHttpUnauthorized(self.c.post(self.list_url + '?user_name=' + self.username + '&api_key=' + self.api_key, data=json.dumps(self.post_data), content_type="application/json"))
+
+    def test_post_list_with_permissions(self):
+        """Post a single device to the API with authenticated and permission"""
+        add_device = Permission.objects.get(codename="add_device")
+        self.user.user_permissions.add(add_device)
+        self.assertEqual(Device.objects.count(), 1)
+        self.assertHttpCreated(self.c.post(self.list_url + '?user_name=' + self.username + '&api_key=' + self.api_key, data=json.dumps(self.post_data), content_type="application/json"))
+        self.assertEqual(Device.objects.count(), 2)
+
+    def test_put_detail_unauthenticated(self):
+        """Try to Put a single device is not allowed from the API with authenticated"""
+        self.assertHttpUnauthorized(self.c.put(self.detail_url))
+
+    def test_put_detail_without_permission(self):
+        """Try to Put a single device is not allowed from the API with authenticated and without permission"""
+        self.assertHttpUnauthorized(self.c.put(self.detail_url, self.get_credentials()))
+
+    def test_put_detail_with_permission(self):
+        """Put a single device is not allowed from the API with authenticated abd permission"""
+        change_device = Permission.objects.get(codename="change_device")
+        self.user.user_permissions.add(change_device)
+        self.assertEqual(Device.objects.count(), 1)
+        self.assertHttpAccepted(self.c.put(self.detail_url + '?user_name=' + self.username + '&api_key=' + self.api_key, data=json.dumps(self.put_data), content_type="application/json"))
+        self.assertEqual(Device.objects.count(), 1)
+        self.assertEqual(Device.objects.get(pk=self.device_1.pk).phone_number, self.put_data.get("phone_number"))
+
+    def test_delete_detail_unauthenticated(self):
+        """Delete a single device is not allowed from the API without authenticated"""
+        self.assertHttpUnauthorized(self.c.delete(self.detail_url))
+
+    def test_delete_detail_without_permission(self):
+        """Delete a single device is not allowed from the API with authenticated and without permission"""
+        self.assertHttpUnauthorized(self.c.delete(self.detail_url, self.get_credentials()))
+
+    def test_delete_detail_with_permission(self):
+        """Delete a single device is not allowed from the API with authenticated and permission"""
+        delete_device = Permission.objects.get(codename="delete_device")
+        self.user.user_permissions.add(delete_device)
+        self.assertHttpAccepted(self.c.delete(self.detail_url, self.get_credentials()))
