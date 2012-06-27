673e97cdb2058df6f4c1673b69f47e5aa0f6d524
Tim Kleinschmidt <timk@tesobe.com>
2012-06-27 20:19:47 +0200

    Adapt all test

--- API/feowl-api/feowl/tests.py
+++ API/feowl-api/feowl/tests.py
@@ -206,10 +206,10 @@ class AreaResourceTest(ResourceTestCase):
         self.assertHttpMethodNotAllowed(self.c.delete(self.detail_url, self.get_credentials()))
 
 
-class UserResourceTest(ResourceTestCase):
+class ContributorResourceTest(ResourceTestCase):
 
     def setUp(self):
-        super(UserResourceTest, self).setUp()
+        super(ContributorResourceTest, self).setUp()
 
         # Create a user.
         self.username = u'john'
@@ -219,25 +219,26 @@ class UserResourceTest(ResourceTestCase):
         self.api_key = self.user.api_key.key
         self.c = Client()
         self.post_data = {
-            'username': 'james',
+            'name': 'james',
             'email': 'james@example.com',
             'password': self.user.__dict__["password"],
-            'profile': {'language': 'DE'}
+            'language': 'DE'
         }
         self.put_data = {
             'email': 'jonny@example.com',
-            'profile': {'language': 'DE'}
+            'language': 'DE'
         }
 
         # Fetch the ``Entry`` object we'll use in testing.
         # Note that we aren't using PKs because they can change depending
         # on what other tests are running.
-        self.user_1 = User.objects.get(pk=1)
+        Contributor(name="Tobias", email="tobias@test.de").save()
+        self.contributor_1 = Contributor.objects.get(pk=1)
 
         # We also build a detail URI, since we will be using it all over.
         # DRY, baby. DRY.
-        self.list_url = '/api/v1/users/'
-        self.detail_url = '{0}{1}/'.format(self.list_url, self.user_1.pk)
+        self.list_url = '/api/v1/contributors/'
+        self.detail_url = '{0}{1}/'.format(self.list_url, self.contributor_1.pk)
 
     def get_credentials(self):
         return {"user_name": self.username, "api_key": self.api_key}
@@ -256,11 +257,10 @@ class UserResourceTest(ResourceTestCase):
         # Here, we're checking an entire structure for the expected data.
         self.assertEqual(self.deserialize(resp)['objects'][0], {
             'id': '1',
-            'username': 'john',
-            'email': 'john@example.com',
+            'name': 'Tobias',
+            'email': 'tobias@test.de',
             'password': settings.DUMMY_PASSWORD,
-            'profile': {'credibility': '1.00', 'language': 'EN'},
-            'resource_uri': '/api/v1/users/1/'
+            'resource_uri': self.detail_url
         })
 
     def test_get_detail_unauthenticated(self):
@@ -273,8 +273,8 @@ class UserResourceTest(ResourceTestCase):
         self.assertValidJSONResponse(resp)
 
         # We use ``assertKeys`` here to just verify the keys, not all the data.
-        self.assertKeys(self.deserialize(resp), ['id', 'username', 'email', 'password', 'profile', 'resource_uri'])
-        self.assertEqual(self.deserialize(resp)['username'], "john")
+        self.assertKeys(self.deserialize(resp), ['id', 'name', 'email', 'password', 'resource_uri'])
+        self.assertEqual(self.deserialize(resp)['name'], "Tobias")
 
     def test_post_list_unauthenticated(self):
         """Try to Post a single user to the API without authenticated"""
@@ -286,11 +286,11 @@ class UserResourceTest(ResourceTestCase):
 
     def test_post_list_with_permissions(self):
         """Try to Post a single user to the API with authenticated and permission"""
-        add_user = Permission.objects.get(codename="add_user")
-        self.user.user_permissions.add(add_user)
-        self.assertEqual(User.objects.count(), 1)
+        add_contributor = Permission.objects.get(codename="add_contributor")
+        self.user.user_permissions.add(add_contributor)
+        self.assertEqual(Contributor.objects.count(), 1)
         self.assertHttpCreated(self.c.post(self.list_url + '?user_name=' + self.username + '&api_key=' + self.api_key, data=json.dumps(self.post_data), content_type="application/json"))
-        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(Contributor.objects.count(), 2)
 
     def test_put_detail_unauthenticated(self):
         """Try to Put a single user is not allowed from the API with authenticated"""
@@ -302,13 +302,12 @@ class UserResourceTest(ResourceTestCase):
 
     def test_put_detail_with_permission(self):
         """Try to Put a single user is not allowed from the API with authenticated abd permission"""
-        change_user = Permission.objects.get(codename="change_user")
-        self.user.user_permissions.add(change_user)
-        self.assertEqual(User.objects.count(), 1)
+        change_contributor = Permission.objects.get(codename="change_contributor")
+        self.user.user_permissions.add(change_contributor)
+        self.assertEqual(Contributor.objects.count(), 1)
         self.assertHttpAccepted(self.c.put(self.detail_url + '?user_name=' + self.username + '&api_key=' + self.api_key, data=json.dumps(self.put_data), content_type="application/json"))
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get(pk=self.user_1.pk).email, self.put_data.get("email"))
-        self.assertEqual(Contributor.objects.get(pk=self.user_1.pk).language, self.put_data.get("profile").get("language"))
+        self.assertEqual(Contributor.objects.count(), 1)
+        self.assertEqual(Contributor.objects.get(pk=self.contributor_1.pk).email, self.put_data.get("email"))
 
     def test_delete_detail_unauthenticated(self):
         """Delete a single user is not allowed from the API without authenticated"""
