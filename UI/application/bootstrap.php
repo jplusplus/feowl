0144f40710e2a1f18d996a226adbdd1d9192361c
Pirhoo <pierre.romera@gmail.com>
2012-06-27 20:39:44 +0200

    Added site constantes and descriptions

--- UI/application/bootstrap.php
+++ UI/application/bootstrap.php
@@ -97,15 +97,15 @@ Kohana::$config->attach(new Config_File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
-	'auth'       => MODPATH.'auth',       // Basic authentication
-	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
-	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-	'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
-	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
+	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
+	'auth'       => MODPATH.'auth',       // Basic authentication
+	'database'   => MODPATH.'database',   // Database access
+	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	'userguide'  => MODPATH.'userguide',  // User guide and API documentation
-    'smarty' 	 => MODPATH.'smarty'	  // Smarty Template Engine
+  'smarty'  	 => MODPATH.'smarty'	  // Smarty Template Engine
 ));
 
 
@@ -124,6 +124,12 @@ if(!in_array($lang, $lang_uris_supported ) ) {
 i18n::lang($lang);
 
 
+// Defines site descriptions
+define("SITE_DESCRIPTION",  __('Feowl empowers local media and their stakeholders with accurate and actionable insights on the electricity supply') );
+define("SITE_URL", 				 'http://feowl.com');
+define("SITE_TITLE",       'Feowl');
+define("SITE_TAGS",        __('feowl, foewl, empowers, power, cut, power cut, accurate, accuration, insights, insight, pool, electricity, cameroon, africa, supply') );
+define("SITE_FB_ADMIN", 	 '686299757');
 
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
