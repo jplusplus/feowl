c7298b6f60cd1c804c6b3b8cb87701a6fecec2b4
pirhoo <pirhoo@sebulba.(none)>
2012-07-03 14:28:34 +0200

    Add an API request to extract the agregated reports between two dates

--- UI/application/classes/controller/json.php
+++ UI/application/classes/controller/json.php
@@ -17,20 +17,41 @@ class Controller_Json extends Controller {
 	public $auto_render = FALSE;
 
 	
-    public function action_index()
-    {
+    public function action_index() {
        //do nothing
          
     }
-	 /**
+
+
+	/**
 	 * input array
-	 * return json encoded array
+	 * @return json encoded array
 	 */
-	public static function action_contribute($json_items)
-	{
-	//@todo post the contents to the api
-	echo json_encode($json_items);
-	
+	public static function action_contribute($json_items) {
+		//@todo post the contents to the api
+		echo json_encode($json_items);	
+	}
+
+	/**
+	 * Display a json string with the reports betweens the given interval
+	 * @access	public	  
+	 */
+	public function action_interval_reports() {
+
+		// Builds the API parameters (first, extracts the username and key)
+		$params  = Kohana::$config->load("apiauth")->get("default");
+		$params += array(
+			"happened_at__gte" => Arr::get($_GET, 'date_gte'),
+			"happened_at__lte" => Arr::get($_GET, 'date_lte'),
+		);
+
+		$restClient = REST_Client::instance();
+		$rep = $restClient->get("aggregation/reports/", $params);		
+
+		// Displays the items in json
+		echo json_encode($rep);
+
+		return $rep;	
 	}
 	
 	
