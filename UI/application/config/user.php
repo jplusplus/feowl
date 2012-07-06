717fc71149cc1f9063f21e047472fc501c918155
naralaurence@mundi.com <Ernest Molua@cidr-a8f9acaa10.(none)>
2012-07-06 23:51:44 +0100

    Added api credential to .gitignore

--- UI/application/config/user.php
+++ UI/application/config/user.php
@@ -1,4 +1,4 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 return array(
-    'username'       => 'wasamundi',    'api_key'         => ''
+    'username'       => 'wasamundi',    'api_key'         => '76315d01a1c968d1830438be794563c0769d7709'
 );  
