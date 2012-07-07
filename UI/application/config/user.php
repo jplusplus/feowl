25be4a6f859325737854e0b57e4496e660655239
Jeanluc Nta <jeanluc.nta@gmail.com>
2012-07-07 09:56:34 +0100

    @feowlTeam provide secret key

--- UI/application/config/user.php
+++ UI/application/config/user.php
@@ -1,4 +1,4 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
-return array(
-    'username'       => 'wasamundi',    'api_key'         => '76315d01a1c968d1830438be794563c0769d7709'
+return array(	/*	*  The api access credentials is provided by @feowlTeam	*/
+    'username'       => '',     'api_key'         => ''
 );  
