4924f13986059361c160e3cf611c9cd7c79458f8
Tim Kleinschmidt <timk@tesobe.com>
2012-07-16 14:34:54 +0200

    Include email templates and adapt the code to it

--- API/feowl-api/feowl/management/commands/newsletter.py
+++ API/feowl-api/feowl/management/commands/newsletter.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.core.mail import EmailMultiAlternatives, get_connection
+from django.template.loader import get_template
+from django.template import Context
 from optparse import make_option
 from feowl.models import Contributor
 import settings
@@ -19,16 +21,19 @@ class Command(BaseCommand):
 
         contributors = Contributor.objects.all().exclude(name=settings.ANONYMOUS_USER_NAME).order_by('-enquiry')[:limit]
         messages = []
+        plaintext = get_template('email.txt')
+        html = get_template('email.html')
+        subject = 'hello'
         connection = get_connection()
         connection.open()
 
         for user in contributors:
             if user.email and user.name:
                 print "{0} - {1} -- {2}".format(user.name, user.email, user.enquiry)
-                subject, from_email, to = 'hello', settings.NEWSLETTER_FROM, user.email
-                text_content = 'This is an important message.'
-                html_content = '<p>This is an <strong>important</strong> message.</p>'
-                msg = EmailMultiAlternatives(subject, text_content, from_email, [to], connection=connection)
+                d = Context({'name': user.name})
+                text_content = plaintext.render(d)
+                html_content = html.render(d)
+                msg = EmailMultiAlternatives(subject, text_content, settings.NEWSLETTER_FROM, [user.email], connection=connection)
                 msg.attach_alternative(html_content, "text/html")
                 messages.append(msg)
 
